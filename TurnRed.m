b9968ec23152e50e13ff47df3a4beb0d12dfdf44
Harrison A. Brown <haabrown@umich.edu>
2015-10-06 16:13:34 -0600

    Added a fix so that the whole row turns red and bolds during an error.

--- TurnRed.m
+++ TurnRed.m
@@ -3,8 +3,10 @@ function [ newdata ] = TurnRed( table, newdata, i )
 %   Turns a cell red and passes the red cells forward
 
 label = newdata(i,1);
+value = newdata(i,2);
 colergen = @(color,text) ['<html><table border=0 width=400 bgcolor=',color,'><TR><TD>',text,'</TD></TR> </table></html>'];
 newdata(i,1) = {colergen('#FF0000',label{1})};
+newdata(i,2) = {colergen('#FF0000', num2str(value{1}))};
 set(table,'data',newdata);
 
 end
