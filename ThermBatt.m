993fb386caf9c93ece63f38ec11821beb320ba15
Harrison A. Brown <haabrown@umich.edu>
2015-10-19 21:01:15 -0600

    Added a small fix for a somehow overlooked type.

--- ThermBatt.m
+++ ThermBatt.m
@@ -1,4 +1,4 @@
 function [ Temp ] = ThermBatt(bytes)
 
-inter = log(2.32/((4095/double(uint16(hex2dec(hex2dec(bytes)))))-1));
+inter = log(2.32/((4095/double(uint16(hex2dec(bytes))))-1));
 Temp = (1/(1/298.15)+(inter/3450))-273.15; % Degrees Celcius
