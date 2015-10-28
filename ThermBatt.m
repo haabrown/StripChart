47f2d6a36ed8fb16eb3ab2a1314971a8e959a921
Harrison <habr6642@colorado,edu>
2015-10-28 14:00:46 -0600

    Made some small bug fixes.

--- ThermBatt.m
+++ ThermBatt.m
@@ -1,4 +1,4 @@
 function [ Temp ] = ThermBatt(bytes)
 
 inter = log(2.32/((4095/double(uint16(bytes)))-1));
-Temp = (1/(1/298.15)+(inter/3450))-273.15; % Degrees Celcius
+Temp = (1/((1/298.15)+(inter/3450)))-273.15; % Degrees Celcius
